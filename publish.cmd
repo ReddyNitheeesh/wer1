69c5f694c8489f6289fb3b497540a3d2eb2f83e6
nitheesh <nitheeshreddy42@gmail.com>
2018-11-11 17:52:39 +0530

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=testingtwo-8344-Web-Deploy.pubxml -p:Password=BChpZmDXgoEpF7ooePZwKmjkqKw3anXFRddpumqndhXpgqleZWLFi4BmpCdT
+
